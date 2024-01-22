2e8e21305f21e740e522b9ada32e43598cc59c6a
franconil <franconil04@gmail.com>
2024-01-22 04:02:55 +0100

    completo el 5 de locoss

--- src/main/kotlin/EJ5.kt
+++ src/main/kotlin/EJ5.kt
@@ -1,5 +1,5 @@
 class Tiempo(hora:Int){
-    var hora = 0
+    private var hora = 0
         set(value) {
             require(value < 24) { "Un dia no tiene mas de 24 horas" }
             field = value
@@ -8,30 +8,35 @@ class Tiempo(hora:Int){
     constructor(hora: Int, min:Int):this(hora){
         this.min = min
     }
+
     constructor(hora: Int, min:Int, seg: Int):this(hora,min ){
         this.seg = seg
     }
 
-    var min = 0
+    private var min = 0
         set(value) {
             this.hora += value / 60
             field = value % 60
         }
 
-    var seg = 0
+    private var seg = 0
         set(value) {
             this.min += value / 60
             field = value % 60
         }
 
+    init {
+        this.hora = hora
+    }
     override fun toString(): String {
-        return "${hora}h, ${min}m, ${seg}s"
+        return "${this.hora}h, ${min}m, ${seg}s"
     }
 
 }
 
 fun main() {
-
-    val hora = Tiempo(0,73,98)
+    val hora = Tiempo(5,73,98)
+    val hora2 = Tiempo(2)
     println(hora.toString())
+    println(hora2.toString())
 }
